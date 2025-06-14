48e3857028a32d124976bb4800d15e6e0058f21a
JoeRodas <166660958+JoeRodas@users.noreply.github.com>
2025-06-14 18:53:25 -0400

    Add TargetConditionals import

--- Sources/include/AppDelegate.h
+++ Sources/include/AppDelegate.h
@@ -1,4 +1,7 @@
+#import <TargetConditionals.h>
+#if !TARGET_OS_OSX
 #import <UIKit/UIKit.h>
+#endif
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
